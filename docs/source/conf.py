e004d6d521a15bef043d62155d71e0eaa0c1002d
Noah Czelusta <83324596+swimninja247@users.noreply.github.com>
2023-04-02 21:51:16 -0400

    HW7: Added documentation (#13)
    * Docs added for user facing code
    
    * Created Sphinx Documentation
    
    * Added installation and example to readme
    
    * Linted and fomatted

---
+++ docs/source/conf.py
@@ -0,0 +1,44 @@
+import sphinx_rtd_theme
+import os
+import sys
+from recommonmark.transform import AutoStructify
+
+
+sys.path.insert(0, os.path.abspath('../..'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GSheetQuery'
+copyright = '2023, Noah Czelusta'
+author = 'Noah Czelusta'
+release = '0.0.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.intersphinx']
+source_suffix = ['.rst']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'auto_toc_tree_section': 'Contents',
+    }, True)
+    app.add_transform(AutoStructify)
--- gsheetquery/Database.py
+++ gsheetquery/Database.py
@@ -2,57 +2,66 @@ import csv
 from gspread.exceptions import WorksheetNotFound
 from gsheetquery import SPREADSHEET_FILENAME_PREFIX
 
-"""
-A spreadsheet file will represent a database
-A sheet will represent a table
-Each row is an entry, and each column is an attribute
-"""
-
 
 class Database:
+    """
+    A class representing a database.  This is the equivalent of a sheets file.
+    """
+
     def __init__(self, spreadsheet) -> None:
+        """
+        Initialize the Database object.
+
+        :param spreadsheet: The spreadsheet object containing the database.
+        """
         self.spreadsheet = spreadsheet
 
-    """
-    Return the canonical name of this database
-    """
-
     def get_name(self):
+        """
+        Return the canonical name of this database.
+
+        :return: The name of the database.
+        """
         return self.spreadsheet.title[len(SPREADSHEET_FILENAME_PREFIX) :]
 
-    """
-    Return a list of the tables (sheets) in this database (spreadsheet)
-    """
-
     def list_tables(self):
+        """
+        Return a list of the tables (sheets) in this database (spreadsheet).
+
+        :return: A list of the table names.
+        """
         return [w.title for w in self.spreadsheet.worksheets()]
 
-    """
-    Create a table with @name
-    """
-
     def add_table(self, name):
+        """
+        Create a table with @name.
+
+        :param name: The name of the table to be created.
+        """
         try:
             self.spreadsheet.worksheet(name)
         except WorksheetNotFound:
             self.spreadsheet.add_worksheet(name, 10, 10)
 
-    """
-    Deletes a table with @name
-    """
-
     def drop_table(self, name):
+        """
+        Deletes a table with @name.
+
+        :param name: The name of the table to be deleted.
+        """
         try:
             worksheet = self.spreadsheet.worksheet(name)
             self.spreadsheet.del_worksheet(worksheet)
         except WorksheetNotFound:
             pass
 
-    """
-    Exports a table to a csv file
-    """
-
     def export_table_csv(self, name, csv_path):
+        """
+        Exports a table to a csv file.
+
+        :param name: The name of the table to be exported.
+        :param csv_path: The path where the csv file will be exported.
+        """
         try:
             worksheet = self.spreadsheet.worksheet(name)
             rows = worksheet.get_all_values()
