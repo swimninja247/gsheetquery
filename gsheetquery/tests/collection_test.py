b20a2559dca0ced87f29f928801dc0144cf9b1d5
swimninja247 <swimninja247@gmail.com>
2023-05-07 13:07:21 -0500

    Added find functionality and passing tests

--- gsheetquery/tests/collection_test.py
+++ gsheetquery/tests/collection_test.py
@@ -9,7 +9,9 @@ class CollectionTests(unittest.TestCase):
         # Create a mock Worksheet object for testing
         self.worksheet = MagicMock(spec=Worksheet)
         self.collection = Collection(self.worksheet)
-        self.doc = {'test': 'test'}
+        self.test_doc = {'test': 'test'}
+        self.test_docs = [['"name": "Alice"', '"age": "25"', '"gender": "F"'], ['"name": "Bob"', '"age": "30"', '"gender": "M"'], ['"name": "Charlie"', '"age": "35"', '"gender": "M"']]
+        self.worksheet.get_values.return_value = self.test_docs
 
     @patch('gsheetquery.utils.doc_to_row')
     def test_insert_one(self, mock_doc_to_row):
@@ -17,9 +19,9 @@ class CollectionTests(unittest.TestCase):
         row_mock = MagicMock()
         mock_doc_to_row.return_value = row_mock
 
-        self.collection.insert_one(self.doc)
+        self.collection.insert_one(self.test_doc)
 
-        mock_doc_to_row.assert_called_once_with(self.doc)
+        mock_doc_to_row.assert_called_once_with(self.test_doc)
         self.worksheet.append_row.assert_called_once_with(row_mock)
 
     @patch('gsheetquery.utils.doc_to_row')
@@ -28,7 +30,43 @@ class CollectionTests(unittest.TestCase):
         row_mock = MagicMock()
         mock_doc_to_row.return_value = row_mock
 
-        self.collection.insert_many([self.doc for _ in range(5)])
+        self.collection.insert_many([self.test_doc for _ in range(5)])
 
         self.assertEqual(mock_doc_to_row.call_count, 5)
         self.worksheet.append_rows.assert_called_once_with([row_mock for _ in range(5)])
+
+    def test_find_one_exists(self):
+        template = {'age': '30'}
+        result = self.collection.find_one(template)
+
+        expected = {'name': 'Bob', 'age': '30', 'gender': 'M'}
+        self.assertEqual(result, expected)
+
+    def test_find_one_not_exists(self):
+        template = {'name': 'Dave'}
+        result = self.collection.find_one(template)
+        self.assertIsNone(result)
+
+    def test_find_many_hit_max(self):
+        template = {'gender': 'M'}
+        result = self.collection.find_many(template, max=1)
+        expected = [
+            {'name': 'Bob', 'age': '30', 'gender': 'M'}
+        ]
+        self.assertEqual(result, expected)
+
+    def test_find_many_found_none(self):
+        template = {'name': 'Dave'}
+        result = self.collection.find_many(template)
+
+        expected = []
+        self.assertEqual(result, expected)
+
+    def test_find_many_found_all(self):
+        template = {'gender': 'M'}
+        result = self.collection.find_many(template)
+        expected = [
+            {'name': 'Bob', 'age': '30', 'gender': 'M'},
+            {'name': 'Charlie', 'age': '35', 'gender': 'M'}
+        ]
+        self.assertEqual(result, expected)
